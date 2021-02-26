615a7f93b21c17705420a7d4e56e9be969349f45
Michael Levan <mlevan1992@gmail.com>
2021-02-26 09:27:28 -0500

    types and variables

---
+++ Module-2-Python-Basics/Types-And-Variables/other_types.py
@@ -0,0 +1,13 @@
+# Types that we'll see throughout this course
+
+#Lists
+fruit = ['Apple', 'Banana', 'Strawberry']
+
+# Range
+number = range(6)
+
+# Dictionary (dict)
+employees = {"Name": "John", "age": 30, "Location": "New Jersey"}
+
+#Bytes
+x = b"Hello"
